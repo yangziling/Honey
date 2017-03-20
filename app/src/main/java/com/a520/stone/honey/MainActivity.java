ebd22fe5da8f643eaf758a5f47d975301f57391c
yangfei <yang_fei@hollywant.com>
2017-03-20 18:50:08 +0800

    Initial commit

---
+++ app/src/main/java/com/a520/stone/honey/MainActivity.java
@@ -0,0 +1,16 @@
+package com.a520.stone.honey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //去掉标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
