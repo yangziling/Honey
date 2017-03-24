e21f092cb0f986d50f9a1513e6a48ed048db6952
yangfei <yang_fei@hollywant.com>
2017-03-24 15:12:04 +0800

    fix(master):添加菜单栏，并抽取xml布局

--- app/src/main/java/com/a520/stone/honey/ForgetPwsActivity.java
+++ app/src/main/java/com/a520/stone/honey/ForgetPwsActivity.java
@@ -6,15 +6,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * <b>Create Date:</b> 2017/3/23<br>
  * <b>Author:</b> Stone <br>
  * <b>Description:</b> <br>
  */
-public class ForgetPwsActivity extends AppCompatActivity {
+public class ForgetPwsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar mToolbar;
+    private TextView mRegister;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, ForgetPwsActivity.class);
@@ -24,12 +27,21 @@ public class ForgetPwsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpsw);
-
-        //toolbar 的显示
-        mToolbar = (Toolbar) findViewById(R.id.forget_toolbar);
-        mToolbar.setTitle("");
-        setSupportActionBar(mToolbar);
+        mRegister = (TextView) findViewById(R.id.text_forget);
+        mRegister.setText("忘记密码");
 
 
     }
+
+    //back返回
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.img_arrow:
+                ForgetPwsActivity.this.finish();
+                break;
+        }
+    }
+
+
 }
