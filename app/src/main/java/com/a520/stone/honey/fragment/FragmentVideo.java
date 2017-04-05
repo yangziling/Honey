067735ef65deb6e4a27a2383782e4b756effafe1
yangfei <yang_fei@hollywant.com>
2017-04-05 16:37:00 +0800

    fix(honey):添加tab 导航条

---
+++ app/src/main/java/com/a520/stone/honey/fragment/FragmentVideo.java
@@ -0,0 +1,25 @@
+package com.a520.stone.honey.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.a520.stone.honey.R;
+
+/**
+ * <b>Create Date:</b> 2017/4/1<br>
+ * <b>Author:</b> Stone <br>
+ * <b>Description:</b> <br>
+ */
+public class FragmentVideo extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_video,container,false);
+    }
+}
